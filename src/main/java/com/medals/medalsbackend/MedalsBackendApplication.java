29579ba4f45aa78bff9f7652e7962ba2190ecad7
janne6565 <jabbekeipert@gmail.com>
2025-01-17 15:10:40 +0100

    add base structure

---
+++ src/main/java/com/medals/medalsbackend/MedalsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medals.medalsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedalsBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MedalsBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/medals/medalsbackend/MedalsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medals.medalsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedalsBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
