733fbe1f144c949fd98c741878416c582c10ac1e
fietensen <fietensen@gmail.com>
2025-01-21 17:53:31 +0100

    add basic athlete service

--- src/main/java/com/medals/medalsbackend/service/AthleteService.java
+++ src/main/java/com/medals/medalsbackend/service/AthleteService.java
@@ -1,7 +1,29 @@
 package com.medals.medalsbackend.service;
 
+import com.medals.medalsbackend.dto.AthleteDTO;
+import com.medals.medalsbackend.entity.Athlete;
+import com.medals.medalsbackend.repository.AthleteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AthleteService {
+    private final AthleteRepository athleteRepository;
+
+    @Autowired
+    public AthleteService(AthleteRepository athleteRepository) {
+        this.athleteRepository = athleteRepository;
+    }
+
+    public boolean addAthlete(AthleteDTO athleteDTO) {
+        Athlete athlete = new Athlete();
+        athlete.setBirthdate(athleteDTO.getBirthdate());
+        athlete.setGender(athleteDTO.getGender());
+        athlete.setEmail(athleteDTO.getEmail());
+        athlete.setLastName(athleteDTO.getLastName());
+        athlete.setFirstName(athleteDTO.getFirstName());
+
+        athleteRepository.save(athlete);
+        return true;
+    }
 }
