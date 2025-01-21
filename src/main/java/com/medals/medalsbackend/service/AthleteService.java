94cec38dfea0c712c8ff5352a0f6b4520b15fec4
fietensen <fietensen@gmail.com>
2025-01-21 18:45:03 +0100

    remove addAthlete method

--- src/main/java/com/medals/medalsbackend/service/AthleteService.java
+++ src/main/java/com/medals/medalsbackend/service/AthleteService.java
@@ -14,16 +14,4 @@ public class AthleteService {
     public AthleteService(AthleteRepository athleteRepository) {
         this.athleteRepository = athleteRepository;
     }
-
-    public boolean addAthlete(AthleteDTO athleteDTO) {
-        Athlete athlete = new Athlete();
-        athlete.setBirthdate(athleteDTO.getBirthdate());
-        athlete.setGender(athleteDTO.getGender());
-        athlete.setEmail(athleteDTO.getEmail());
-        athlete.setLastName(athleteDTO.getLastName());
-        athlete.setFirstName(athleteDTO.getFirstName());
-
-        athleteRepository.save(athlete);
-        return true;
-    }
 }
