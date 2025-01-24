92fa63229152e86edd5c61939dd858231b45fd75
fietensen <fietensen@gmail.com>
2025-01-24 09:29:10 +0100

    change repository type

--- src/main/java/com/medals/medalsbackend/repository/AthleteRepository.java
+++ src/main/java/com/medals/medalsbackend/repository/AthleteRepository.java
@@ -1,9 +1,9 @@
 package com.medals.medalsbackend.repository;
 
 import com.medals.medalsbackend.entity.Athlete;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AthleteRepository extends CrudRepository<Athlete, Long> {
+public interface AthleteRepository extends JpaRepository<Athlete, String> {
 }
