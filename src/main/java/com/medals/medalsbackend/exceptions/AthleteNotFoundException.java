ad5c29e040e2bf562ef4ac8012bfafd298c163ff
Janne Keipert <janne.keipert@telekom.de>
2025-01-29 15:57:38 +0100

    http status refactor

--- src/main/java/com/medals/medalsbackend/exceptions/AthleteNotFoundException.java
+++ src/main/java/com/medals/medalsbackend/exceptions/AthleteNotFoundException.java
@@ -1,10 +1,11 @@
 package com.medals.medalsbackend.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
 public class AthleteNotFoundException extends GenericAPIRequestException {
     AthleteNotFoundException(Long athleteId) {
-        super("Athlete with id not found [id: " + athleteId + "]", HttpStatusCode.valueOf(404));
+        super("Athlete with id not found [id: " + athleteId + "]", HttpStatus.NOT_FOUND);
     }
 
     public static AthleteNotFoundException fromAthleteId(Long athleteId) {
