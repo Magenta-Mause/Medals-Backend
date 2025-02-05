7446b9bcf7f2cd04dd55e356c104ce44407214cf
janne6565 <jabbekeipert@gmail.com>
2025-02-05 17:59:03 +0100

    add firstName and lastName to userEntity entity

--- src/main/java/com/medals/medalsbackend/entity/users/Athlete.java
+++ src/main/java/com/medals/medalsbackend/entity/users/Athlete.java
@@ -19,15 +19,6 @@ import java.time.LocalDate;
 @DiscriminatorValue("ATHLETE")
 @SuperBuilder
 public class Athlete extends UserEntity {
-
-    @Column(name = "first_name", nullable = false)
-    @JsonProperty("first_name")
-    private String firstName;
-
-    @Column(name = "last_name", nullable = false)
-    @JsonProperty("last_name")
-    private String lastName;
-
     @Column(nullable = false)
     private LocalDate birthdate;
 
--- src/main/java/com/medals/medalsbackend/entity/users/Trainer.java
+++ src/main/java/com/medals/medalsbackend/entity/users/Trainer.java
@@ -18,11 +18,5 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @SuperBuilder
 public class Trainer extends UserEntity {
-    @Column(name = "first_name", nullable = false)
-    @JsonProperty("first_name")
-    private String firstName;
-    @Column(name = "last_name", nullable = false)
-    @JsonProperty("last_name")
-    private String lastName;
     protected String type = "TRAINER";
 }
