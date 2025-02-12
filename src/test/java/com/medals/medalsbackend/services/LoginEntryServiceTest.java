82fd6ddb1f9f18b4cb66d226344bd050ea57a369
janne6565 <jabbekeipert@gmail.com>
2025-02-12 16:04:07 +0100

    push image to preprod

--- src/test/java/com/medals/medalsbackend/services/LoginEntryServiceTest.java
+++ src/test/java/com/medals/medalsbackend/services/LoginEntryServiceTest.java
@@ -35,119 +35,119 @@ import static org.mockito.Mockito.*;
 @ActiveProfiles("test")
 public class LoginEntryServiceTest {
 
-    @Mock
-    private JwtService jwtService;
-    @Mock
-    private LoginEntryRepository loginEntryRepository;
-    @Mock
-    private OneTimeCodeService oneTimeCodeService;
-    @Mock
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @InjectMocks
-    private LoginEntryService loginEntryService;
+  @Mock
+  private JwtService jwtService;
+  @Mock
+  private LoginEntryRepository loginEntryRepository;
+  @Mock
+  private OneTimeCodeService oneTimeCodeService;
+  @Mock
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+  @InjectMocks
+  private LoginEntryService loginEntryService;
 
-    @Test
-    @SneakyThrows
-    public void testPasswordReset() {
-        when(oneTimeCodeService.getEmailFromOneTimeCode(eq("test"), eq(OneTimeCodeType.SET_PASSWORD))).thenReturn("admin@example.org");
-        when(loginEntryRepository.findById(eq("admin@example.org"))).thenReturn(Optional.of(LoginEntry.builder().email("admin@example.org").password("oldPassword").build()));
-        loginEntryService.createLoginEntry("admin@example.org");
-        loginEntryService.setPassword("test", "newPassword");
+  @Test
+  @SneakyThrows
+  public void testPasswordReset() {
+    when(oneTimeCodeService.getEmailFromOneTimeCode(eq("test"), eq(OneTimeCodeType.SET_PASSWORD))).thenReturn("admin@example.org");
+    when(loginEntryRepository.findById(eq("admin@example.org"))).thenReturn(Optional.of(LoginEntry.builder().email("admin@example.org").password("oldPassword").build()));
+    loginEntryService.createLoginEntry("admin@example.org");
+    loginEntryService.setPassword("test", "newPassword");
 
-        ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
-        verify(loginEntryRepository, times(2)).save(loginEntryArgumentCaptor.capture());
-        assertEquals("admin@example.org", loginEntryArgumentCaptor.getValue().getEmail());
-    }
+    ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
+    verify(loginEntryRepository, times(2)).save(loginEntryArgumentCaptor.capture());
+    assertEquals("admin@example.org", loginEntryArgumentCaptor.getValue().getEmail());
+  }
 
-    @Test
-    @SneakyThrows
-    public void testLoginEntryCreationTriggersEmailSending() {
-        when(loginEntryRepository.existsById(any())).thenReturn(false);
-        loginEntryService.createLoginEntry("test@gmail.com");
-        verify(oneTimeCodeService, times(1)).createSetPasswordToken(eq("test@gmail.com"));
-    }
+  @Test
+  @SneakyThrows
+  public void testLoginEntryCreationTriggersEmailSending() {
+    when(loginEntryRepository.existsById(any())).thenReturn(false);
+    loginEntryService.createLoginEntry("test@gmail.com");
+    verify(oneTimeCodeService, times(1)).createSetPasswordToken(eq("test@gmail.com"));
+  }
 
-    @Test
-    @SneakyThrows
-    public void testSetPasswordTokenValidity() {
-        when(oneTimeCodeService.getEmailFromOneTimeCode(any(), any())).thenReturn("test@gmail.com");
-        when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.of(LoginEntry.builder().email("test@gmail.com").build()));
-        when(bCryptPasswordEncoder.encode(any())).thenReturn("encryptedPassword");
+  @Test
+  @SneakyThrows
+  public void testSetPasswordTokenValidity() {
+    when(oneTimeCodeService.getEmailFromOneTimeCode(any(), any())).thenReturn("test@gmail.com");
+    when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.of(LoginEntry.builder().email("test@gmail.com").build()));
+    when(bCryptPasswordEncoder.encode(any())).thenReturn("encryptedPassword");
 
-        ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
+    ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
 
-        loginEntryService.setPassword("test", "newPassword");
-        verify(loginEntryRepository, times(1)).findById("test@gmail.com");
-        verify(loginEntryRepository, times(1)).save(loginEntryArgumentCaptor.capture());
+    loginEntryService.setPassword("test", "newPassword");
+    verify(loginEntryRepository, times(1)).findById("test@gmail.com");
+    verify(loginEntryRepository, times(1)).save(loginEntryArgumentCaptor.capture());
 
-        assertEquals("test@gmail.com", loginEntryArgumentCaptor.getValue().getEmail());
-        assertEquals("encryptedPassword", loginEntryArgumentCaptor.getValue().getPassword());
-    }
+    assertEquals("test@gmail.com", loginEntryArgumentCaptor.getValue().getEmail());
+    assertEquals("encryptedPassword", loginEntryArgumentCaptor.getValue().getPassword());
+  }
 
 
-    @Test
-    @SneakyThrows
-    public void testCheckLogin() {
-        when(loginEntryRepository.findById(eq("test@gmail.com"))).thenReturn(Optional.of(LoginEntry.builder().password("encryptedPassword").email("test@gmail.com").build()));
-        when(bCryptPasswordEncoder.matches(eq("testPassword"), eq("encryptedPassword"))).thenReturn(true);
+  @Test
+  @SneakyThrows
+  public void testCheckLogin() {
+    when(loginEntryRepository.findById(eq("test@gmail.com"))).thenReturn(Optional.of(LoginEntry.builder().password("encryptedPassword").email("test@gmail.com").build()));
+    when(bCryptPasswordEncoder.matches(eq("testPassword"), eq("encryptedPassword"))).thenReturn(true);
 
-        LoginEntry result = loginEntryService.checkLogin("test@gmail.com", "testPassword");
-        assertEquals("test@gmail.com", result.getEmail());
-    }
+    LoginEntry result = loginEntryService.checkLogin("test@gmail.com", "testPassword");
+    assertEquals("test@gmail.com", result.getEmail());
+  }
 
-    @Test
-    @SneakyThrows
-    public void testBuildJwtRefreshToken() {
-        when(loginEntryRepository.findById(eq("test@gmail.com"))).thenReturn(Optional.of(LoginEntry.builder().password("encryptedPassword").email("test@gmail.com").build()));
-        when(bCryptPasswordEncoder.matches(eq("testPassword"), eq("encryptedPassword"))).thenReturn(true);
-        loginEntryService.buildJwtRefreshToken("test@gmail.com", "testPassword");
+  @Test
+  @SneakyThrows
+  public void testBuildJwtRefreshToken() {
+    when(loginEntryRepository.findById(eq("test@gmail.com"))).thenReturn(Optional.of(LoginEntry.builder().password("encryptedPassword").email("test@gmail.com").build()));
+    when(bCryptPasswordEncoder.matches(eq("testPassword"), eq("encryptedPassword"))).thenReturn(true);
+    loginEntryService.buildJwtRefreshToken("test@gmail.com", "testPassword");
 
-        ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
-        verify(jwtService, times(1)).buildRefreshToken(loginEntryArgumentCaptor.capture());
+    ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
+    verify(jwtService, times(1)).buildRefreshToken(loginEntryArgumentCaptor.capture());
 
-        assertEquals("test@gmail.com", loginEntryArgumentCaptor.getValue().getEmail());
-        assertEquals("encryptedPassword", loginEntryArgumentCaptor.getValue().getPassword());
-    }
+    assertEquals("test@gmail.com", loginEntryArgumentCaptor.getValue().getEmail());
+    assertEquals("encryptedPassword", loginEntryArgumentCaptor.getValue().getPassword());
+  }
 
-    @Test
-    @SneakyThrows
-    public void testAddUserToLoginEntry() {
-        when(loginEntryRepository.existsById("test@gmail.com")).thenReturn(true);
-        when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.of(LoginEntry.builder().email("test@gmail.com").users(new ArrayList<>()).build()));
-        loginEntryService.addUserToLogin("test@gmail.com", Admin.builder().email("test@gmail.com").firstName("AdminFirstName").lastName("AdminLastName").build());
-        ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
-        verify(loginEntryRepository, times(1)).save(loginEntryArgumentCaptor.capture());
-        assertEquals("test@gmail.com", loginEntryArgumentCaptor.getValue().getEmail());
-        List<UserEntity> users = loginEntryArgumentCaptor.getValue().getUsers();
-        assertEquals(1, users.size());
-        UserEntity user = users.getFirst();
-        assertEquals("AdminFirstName", user.getFirstName());
-        assertEquals("AdminLastName", user.getLastName());
-    }
+  @Test
+  @SneakyThrows
+  public void testAddUserToLoginEntry() {
+    when(loginEntryRepository.existsById("test@gmail.com")).thenReturn(true);
+    when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.of(LoginEntry.builder().email("test@gmail.com").users(new ArrayList<>()).build()));
+    loginEntryService.addUserToLogin("test@gmail.com", Admin.builder().email("test@gmail.com").firstName("AdminFirstName").lastName("AdminLastName").build());
+    ArgumentCaptor<LoginEntry> loginEntryArgumentCaptor = ArgumentCaptor.forClass(LoginEntry.class);
+    verify(loginEntryRepository, times(1)).save(loginEntryArgumentCaptor.capture());
+    assertEquals("test@gmail.com", loginEntryArgumentCaptor.getValue().getEmail());
+    List<UserEntity> users = loginEntryArgumentCaptor.getValue().getUsers();
+    assertEquals(1, users.size());
+    UserEntity user = users.getFirst();
+    assertEquals("AdminFirstName", user.getFirstName());
+    assertEquals("AdminLastName", user.getLastName());
+  }
 
-    @Test
-    public void testAddUserToLoginEmailDoesntExistException() {
-        when(loginEntryRepository.existsById("test@gmail.com")).thenReturn(false);
-        assertThrows(EmailDoesntExistException.class, () -> loginEntryService.addUserToLogin("test@gmail.com", Athlete.builder().build()));
-    }
+  @Test
+  public void testAddUserToLoginEmailDoesntExistException() {
+    when(loginEntryRepository.existsById("test@gmail.com")).thenReturn(false);
+    assertThrows(EmailDoesntExistException.class, () -> loginEntryService.addUserToLogin("test@gmail.com", Athlete.builder().build()));
+  }
 
-    @Test
-    public void testUserLoginDoesntExistException() {
-        when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.empty());
-        assertThrows(EmailDoesntExistException.class, () -> loginEntryService.checkLogin("test@gmail.com", "testPassword"));
-    }
+  @Test
+  public void testUserLoginDoesntExistException() {
+    when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.empty());
+    assertThrows(EmailDoesntExistException.class, () -> loginEntryService.checkLogin("test@gmail.com", "testPassword"));
+  }
 
-    @Test
-    public void testUserLoginDoesntMatchException() {
-        when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.ofNullable(LoginEntry.builder().email("test@gmail.com").password("encryptedPassword").build()));
-        when(bCryptPasswordEncoder.matches(eq("testPassword"), eq("encryptedPassword"))).thenReturn(false);
-        assertThrows(LoginDoesntMatchException.class, () -> loginEntryService.checkLogin("test@gmail.com", "testPassword"));
-    }
+  @Test
+  public void testUserLoginDoesntMatchException() {
+    when(loginEntryRepository.findById("test@gmail.com")).thenReturn(Optional.ofNullable(LoginEntry.builder().email("test@gmail.com").password("encryptedPassword").build()));
+    when(bCryptPasswordEncoder.matches(eq("testPassword"), eq("encryptedPassword"))).thenReturn(false);
+    assertThrows(LoginDoesntMatchException.class, () -> loginEntryService.checkLogin("test@gmail.com", "testPassword"));
+  }
 
-    @SneakyThrows
-    @Test
-    public void testSetPasswordThrowsException() {
-        when(oneTimeCodeService.getEmailFromOneTimeCode(eq("testToken"), eq(OneTimeCodeType.SET_PASSWORD))).thenThrow(OneTimeCodeExpiredException.class);
-        assertThrows(OneTimeCodeExpiredException.class, () -> loginEntryService.setPassword("testToken", "newPassword"));
-    }
+  @SneakyThrows
+  @Test
+  public void testSetPasswordThrowsException() {
+    when(oneTimeCodeService.getEmailFromOneTimeCode(eq("testToken"), eq(OneTimeCodeType.SET_PASSWORD))).thenThrow(OneTimeCodeExpiredException.class);
+    assertThrows(OneTimeCodeExpiredException.class, () -> loginEntryService.setPassword("testToken", "newPassword"));
+  }
 }
