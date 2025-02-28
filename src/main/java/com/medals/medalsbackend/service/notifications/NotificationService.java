cb477c8231df1bb096e106e7c45b66894c9f9490
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-02-28 17:53:55 +0100

    change endpoint of createAccount and inviteTrainer Notifications

--- src/main/java/com/medals/medalsbackend/service/notifications/NotificationService.java
+++ src/main/java/com/medals/medalsbackend/service/notifications/NotificationService.java
@@ -24,13 +24,13 @@ public class NotificationService {
     }
 
     public void sendCreateAccountNotification(String email, String oneTimeCode) {
-        String link = configuration.getNotificationServiceBaseUrl() + "/resetPassword?oneTimeCode=" + oneTimeCode;
+        String link = configuration.getNotificationServiceBaseUrl() + "/setPassword?oneTimeCode=" + oneTimeCode;
         String text = templateService.generateSetPasswordNotification(email, link);
         mailService.sendEmail(email, "Medals - Account Creation", text);
     }
 
     public void sendInviteTrainerNotification(String email, String oneTimeCode) {
-        String link = configuration.getNotificationServiceBaseUrl() + "/resetPassword?oneTimeCode=" + oneTimeCode;
+        String link = configuration.getNotificationServiceBaseUrl() + "/setPassword?oneTimeCode=" + oneTimeCode;
         String text = templateService.generateInviteTrainerNotification(email, link);
         mailService.sendEmail(email, "Medals - Trainer Creation", text);
     }
