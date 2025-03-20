775f5e61c9ea39fc915ca2f944b47598d30a7be6
py-bay <github.com.tribunal325@passmail.net>
2025-03-20 14:35:23 +0100

    cleanup

--- src/main/java/com/medals/medalsbackend/service/performancerecording/DisciplineService.java
+++ src/main/java/com/medals/medalsbackend/service/performancerecording/DisciplineService.java
@@ -15,7 +15,6 @@ import com.medals.medalsbackend.service.websockets.DisciplineWebsocketMessagingS
 import com.medals.medalsbackend.service.websockets.RatingMetricWebsocketMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
