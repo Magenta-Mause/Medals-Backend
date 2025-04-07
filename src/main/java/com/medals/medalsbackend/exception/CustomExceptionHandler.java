32614f7258660817cfb0a24b27d155226d9cdda9
Janne <jabbekeipert@gmail.com>
2025-04-07 15:22:44 +0200

    resolve PR comments

--- src/main/java/com/medals/medalsbackend/exception/CustomExceptionHandler.java
+++ src/main/java/com/medals/medalsbackend/exception/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.medals.medalsbackend.exception;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
@@ -21,49 +22,51 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> internalErrorHandler(Exception ex, WebRequest request) {
-        log.error(ex.getMessage(), ex);
-        return ResponseEntity.internalServerError().body("Internal Server Error");
-    }
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> internalErrorHandler(Exception ex, WebRequest request) {
+		log.error(ex.getMessage(), ex);
+		return ResponseEntity.internalServerError().body("Internal Server Error");
+	}
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleUnrecognizedPropertyException(HttpMessageNotReadableException ex, WebRequest request) {
-        try {
-            UnrecognizedPropertyException exception = (UnrecognizedPropertyException) ex.getCause();
-            return ResponseEntity.badRequest().body("Malformed Json, invalid property: '" + exception.getPropertyName() + "'");
-        } catch (Exception ignored) {
-            return ResponseEntity.badRequest().body("Message not readable");
-        }
-    }
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<String> handleUnrecognizedPropertyException(HttpMessageNotReadableException ex, WebRequest request) {
+		try {
+			UnrecognizedPropertyException exception = (UnrecognizedPropertyException) ex.getCause();
+			return ResponseEntity.badRequest()
+					.contentType(MediaType.APPLICATION_JSON)
+					.body("Malformed Json, invalid property: '" + exception.getPropertyName() + "'");
+		} catch (Exception ignored) {
+			return ResponseEntity.badRequest().body("Message not readable");
+		}
+	}
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e, WebRequest request) {
-        Map<String, String> errors = e.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, fieldError -> {
-            String defaultMessage = fieldError.getDefaultMessage();
-            return defaultMessage != null ? defaultMessage : "No error message available";
-        }));
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e, WebRequest request) {
+		Map<String, String> errors = e.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, fieldError -> {
+			String defaultMessage = fieldError.getDefaultMessage();
+			return defaultMessage != null ? defaultMessage : "No error message available";
+		}));
 
-        return ResponseEntity.badRequest().body(errors);
-    }
+		return ResponseEntity.badRequest().body(errors);
+	}
 
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
 
-    @ExceptionHandler(MissingRequestCookieException.class)
-    public ResponseEntity<String> handleMissingCookieException(MissingRequestCookieException e, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing Cookie '" + e.getCookieName() + "'");
-    }
+	@ExceptionHandler(MissingRequestCookieException.class)
+	public ResponseEntity<String> handleMissingCookieException(MissingRequestCookieException e, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing Cookie '" + e.getCookieName() + "'");
+	}
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<String> handleMissingServletRequestParameterException(MissingServletRequestParameterException e, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing Parameter '" + e.getParameterName() + "'");
-    }
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<String> handleMissingServletRequestParameterException(MissingServletRequestParameterException e, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing Parameter '" + e.getParameterName() + "'");
+	}
 
-    @ExceptionHandler(GenericAPIRequestException.class)
-    public ResponseEntity<String> genericAPIRequestException(GenericAPIRequestException ex, WebRequest request) {
-        return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
-    }
+	@ExceptionHandler(GenericAPIRequestException.class)
+	public ResponseEntity<String> genericAPIRequestException(GenericAPIRequestException ex, WebRequest request) {
+		return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
+	}
 }
