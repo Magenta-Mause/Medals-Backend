0c6ecdf936bfc4461cb864d5dec478a4709d225c
Janne <60547730+Janne6565@users.noreply.github.com>
2025-04-08 16:02:33 +0200

    [MED-139] implement admin creation (#30)
    * implement admin deletion
    
    * fail safe HttpMessageNotReadableException
    
    * remove private email
    
    * remove unused config changes
    
    * resolve PR comments

--- src/main/java/com/medals/medalsbackend/controller/admin/AdminController.java
+++ src/main/java/com/medals/medalsbackend/controller/admin/AdminController.java
@@ -1,8 +1,16 @@
 package com.medals.medalsbackend.controller.admin;
 
 
+import com.medals.medalsbackend.dto.AdminCreationDto;
+import com.medals.medalsbackend.entity.users.Admin;
+import com.medals.medalsbackend.entity.users.UserType;
 import com.medals.medalsbackend.exception.AdminNotFoundException;
+import com.medals.medalsbackend.exception.InternalException;
+import com.medals.medalsbackend.service.authorization.AuthorizationService;
+import com.medals.medalsbackend.service.authorization.ForbiddenException;
+import com.medals.medalsbackend.service.authorization.NoAuthenticationFoundException;
 import com.medals.medalsbackend.service.user.AdminService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +19,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.medals.medalsbackend.controller.BaseController.BASE_PATH;
 
@@ -19,12 +30,24 @@ import static com.medals.medalsbackend.controller.BaseController.BASE_PATH;
 @RequiredArgsConstructor
 public class AdminController {
 
-	private final AdminService adminService;
+    private final AdminService adminService;
+    private final AuthorizationService authorizationService;
 
-	@DeleteMapping("/{adminId}")
-	@PreAuthorize("hasRole('admin')")
-	public ResponseEntity<Void> deleteAdmin(@PathVariable Long adminId) throws AdminNotFoundException {
-		adminService.deleteAdmin(adminId);
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
-	}
+    @DeleteMapping("/{adminId}")
+    public ResponseEntity<Void> deleteAdmin(@PathVariable Long adminId) throws AdminNotFoundException, ForbiddenException, NoAuthenticationFoundException {
+        authorizationService.assertRoleIn(List.of(UserType.ADMIN));
+        adminService.deleteAdmin(adminId);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<Admin> createAdmin(@RequestBody @Valid AdminCreationDto admin) throws ForbiddenException, NoAuthenticationFoundException, InternalException {
+        authorizationService.assertRoleIn(List.of(UserType.ADMIN));
+        return ResponseEntity.status(HttpStatus.CREATED).body(adminService.createAdmin(Admin.builder()
+            .firstName(admin.getFirstName())
+            .lastName(admin.getLastName())
+            .email(admin.getEmail())
+            .type(UserType.ADMIN)
+            .build()));
+    }
 }
--- src/main/java/com/medals/medalsbackend/exception/CustomExceptionHandler.java
+++ src/main/java/com/medals/medalsbackend/exception/CustomExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.medals.medalsbackend.exception;
 
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
@@ -19,39 +22,51 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> internalErrorHandler(Exception ex, WebRequest request) {
-        log.error(ex.getMessage(), ex);
-        return ResponseEntity.internalServerError().body("Internal Server Error");
-    }
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> internalErrorHandler(Exception ex, WebRequest request) {
+		log.error(ex.getMessage(), ex);
+		return ResponseEntity.internalServerError().body("Internal Server Error");
+	}
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e, WebRequest request) {
-        Map<String, String> errors = e.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, fieldError -> {
-            String defaultMessage = fieldError.getDefaultMessage();
-            return defaultMessage != null ? defaultMessage : "No error message available";
-        }));
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<String> handleUnrecognizedPropertyException(HttpMessageNotReadableException ex, WebRequest request) {
+		try {
+			UnrecognizedPropertyException exception = (UnrecognizedPropertyException) ex.getCause();
+			return ResponseEntity.badRequest()
+					.contentType(MediaType.APPLICATION_JSON)
+					.body("Malformed Json, invalid property: '" + exception.getPropertyName() + "'");
+		} catch (Exception ignored) {
+			return ResponseEntity.badRequest().body("Message not readable");
+		}
+	}
 
-        return ResponseEntity.badRequest().body(errors);
-    }
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e, WebRequest request) {
+		Map<String, String> errors = e.getBindingResult().getFieldErrors().stream().collect(Collectors.toMap(FieldError::getField, fieldError -> {
+			String defaultMessage = fieldError.getDefaultMessage();
+			return defaultMessage != null ? defaultMessage : "No error message available";
+		}));
 
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
+		return ResponseEntity.badRequest().body(errors);
+	}
 
-    @ExceptionHandler(MissingRequestCookieException.class)
-    public ResponseEntity<String> handleMissingCookieException(MissingRequestCookieException e, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing Cookie '" + e.getCookieName() + "'");
-    }
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<String> handleMissingServletRequestParameterException(MissingServletRequestParameterException e, WebRequest request) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing Parameter '" + e.getParameterName() + "'");
-    }
+	@ExceptionHandler(MissingRequestCookieException.class)
+	public ResponseEntity<String> handleMissingCookieException(MissingRequestCookieException e, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing Cookie '" + e.getCookieName() + "'");
+	}
 
-    @ExceptionHandler(GenericAPIRequestException.class)
-    public ResponseEntity<String> genericAPIRequestException(GenericAPIRequestException ex, WebRequest request) {
-        return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
-    }
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<String> handleMissingServletRequestParameterException(MissingServletRequestParameterException e, WebRequest request) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing Parameter '" + e.getParameterName() + "'");
+	}
+
+	@ExceptionHandler(GenericAPIRequestException.class)
+	public ResponseEntity<String> genericAPIRequestException(GenericAPIRequestException ex, WebRequest request) {
+		return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
+	}
 }
