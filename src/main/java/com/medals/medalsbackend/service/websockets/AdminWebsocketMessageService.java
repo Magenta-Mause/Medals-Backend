bd9a5d66c47b9bce6b336109aca10fe385b70e0e
py-bay <github.com.tribunal325@passmail.net>
2025-05-04 23:01:12 +0200

    add update and create to AdminWebsocketMessageService

--- src/main/java/com/medals/medalsbackend/service/websockets/AdminWebsocketMessageService.java
+++ src/main/java/com/medals/medalsbackend/service/websockets/AdminWebsocketMessageService.java
@@ -1,6 +1,6 @@
 package com.medals.medalsbackend.service.websockets;
 
-
+import com.medals.medalsbackend.entity.users.Admin;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,15 @@ public class AdminWebsocketMessageService {
 
     private final SimpMessagingTemplate messagingTemplate;
 
-    public void sendAdminDelete(Long adminId){
+    public void sendAdminDelete(Long adminId) {
         messagingTemplate.convertAndSend("/topics/admin/deletion", adminId);
+    }
+
+    public void sendAdminUpdate(Admin admin) {
+        messagingTemplate.convertAndSend("/topics/admin/update", admin);
+    }
+
+    public void sendAdminCreate(Admin admin) {
+        messagingTemplate.convertAndSend("/topics/admin/creation", admin);
     }
 }
