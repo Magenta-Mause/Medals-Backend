09a9246b28b3a6ab9bf49b2771f34a2e58454d9d
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2025-05-12 20:08:39 +0200

    comment: added a comment

--- src/main/java/com/medals/medalsbackend/controller/trainer/TrainerController.java
+++ src/main/java/com/medals/medalsbackend/controller/trainer/TrainerController.java
@@ -90,7 +90,8 @@ public class TrainerController {
 
 	@DeleteMapping(value = "/trainer-athlete-connection")
 	public ResponseEntity<Void> removeTrainerAthleteConnection(@RequestParam Long trainerId, @RequestParam Long athleteId) throws Exception {
-		//authorizationService.assertUserHasOwnerAccess(trainerId);
+		// add authorization so both athlete and trainer are able to use this endpoint
+		authorizationService.assertUserHasOwnerAccess(trainerId);
 		athleteService.removeConnection(trainerId, athleteId);
 		return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
 	}
