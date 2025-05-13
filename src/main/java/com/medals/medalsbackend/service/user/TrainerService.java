6e5cd645ff2846e1d9bce2643838d1a7c84b96c2
py-bay <github.com.tribunal325@passmail.net>
2025-05-13 22:34:10 +0200

    add SYSTEM placeholder again as admin name

--- src/main/java/com/medals/medalsbackend/service/user/TrainerService.java
+++ src/main/java/com/medals/medalsbackend/service/user/TrainerService.java
@@ -71,7 +71,7 @@ public class TrainerService {
     }
 
     public Trainer createTrainer(Trainer trainer) throws InternalException {
-        return createTrainer(trainer, "the Medals Team");
+        return createTrainer(trainer, "SYSTEM");
     }
 
     public UserEntity insertTrainer(TrainerDto trainerDto, String adminName) throws InternalException {
