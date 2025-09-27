09efcd91d88d94b44e327ca1a288e9fa79704085
Janne <jabbekeipert@gmail.com>
2025-09-27 20:50:28 +0200

    **Refactor email service: replace Feign client with WebClient implementation** Replaced MailClient interface with WebClient for sending emails. Updated email-related configuration properties. Removed no longer used configurations and dependencies. Added WebFlux dependency for the new implementation.

--- src/main/java/com/medals/medalsbackend/service/notifications/mail/mailApi/MailServiceConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.medals.medalsbackend.service.notifications.mail.mailApi;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "app.email")
-public record MailServiceConfiguration(String author) {
-}
--- src/main/java/com/medals/medalsbackend/service/notifications/mail/mailApi/MailServiceImpl.java
+++ src/main/java/com/medals/medalsbackend/service/notifications/mail/mailApi/MailServiceImpl.java
@@ -1,30 +1,49 @@
 package com.medals.medalsbackend.service.notifications.mail.mailApi;
 
-import com.medals.medalsbackend.client.mail.MailClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.medals.medalsbackend.service.notifications.mail.MailService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-@EnableConfigurationProperties(MailServiceConfiguration.class)
 public class MailServiceImpl implements MailService {
-    private final MailServiceConfiguration configuration;
-    private final MailClient mailClient;
-    private final Environment environment;
+	private final Environment environment;
+	private final ObjectMapper objectMapper;
+	@Value("${app.mailClient.url}")
+	private String mailClientUrl;
+	@Value("${app.mailClient.auth-key}")
+	private String mailClientAuthKey;
 
-    @Override
-    public void sendEmail(String receiver, String subject, String message) {
-        if (Arrays.stream(environment.getActiveProfiles()).toList().contains("test")) {
-            return;
-        }
-        mailClient.sendMail(configuration.author(), receiver, message, subject);
-    }
+	@SneakyThrows
+	@Override
+	public void sendEmail(String receiver, String subject, String message) {
+		if (Arrays.stream(environment.getActiveProfiles()).toList().contains("test")) {
+			return;
+		}
+		WebClient webClient = WebClient.create();
+		Map<String, String> body = Map.of(
+				"recipient", receiver,
+				"subject", subject,
+				"body", message,
+				"enableHtml", "true"
+		);
+
+		webClient.post().uri(mailClientUrl + "/mail")
+				.bodyValue(body)
+				.header(
+						"Authorization", mailClientAuthKey
+				).retrieve()
+				.toBodilessEntity()
+				.block();
+	}
 }
